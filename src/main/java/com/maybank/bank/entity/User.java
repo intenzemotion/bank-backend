63af070fb3e22d27f5fbca1a75d90ea66c5a566d
Ikram <wsuaimi@gmail.com>
2024-01-18 09:13:59 +0800

    chore: init User entity

---
+++ src/main/java/com/maybank/bank/entity/User.java
@@ -0,0 +1,43 @@
+package com.maybank.bank.entity;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String accountNumber; // generated on creation
+    private BigDecimal accountBalance;
+    private String status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+
+}
