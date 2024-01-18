ce0aecf79179f42c368ffab6340a6482c60f6a08
Ikram <wsuaimi@gmail.com>
2024-01-18 10:29:51 +0800

    chore: created simple user controller

---
+++ src/main/java/com/maybank/bank/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.maybank.bank.controller;
+
+import com.maybank.bank.dto.BankResponse;
+import com.maybank.bank.dto.UserRequest;
+import com.maybank.bank.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public BankResponse createAccount(@RequestBody UserRequest userRequest) {
+        return userService.createAccount(userRequest);
+    }
+}
