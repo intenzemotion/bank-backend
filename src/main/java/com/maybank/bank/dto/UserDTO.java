4be332a15109f6ef5a5034abed492bc4e4511cfe
Ikram <wsuaimi@gmail.com>
2024-01-18 09:34:07 +0800

    chore: created skeleton userDTO and userRepo

---
+++ src/main/java/com/maybank/bank/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.maybank.bank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String status;
+
+}
