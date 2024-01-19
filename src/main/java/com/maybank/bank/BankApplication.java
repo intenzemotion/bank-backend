d19ff69c412741961fbe9073d5d248a73ffc5179
Ikram <wsuaimi@gmail.com>
2024-01-19 17:46:41 +0800

    docs: added openAPI (Swagger) for User APIs

--- src/main/java/com/maybank/bank/BankApplication.java
+++ src/main/java/com/maybank/bank/BankApplication.java
@@ -1,13 +1,38 @@
 package com.maybank.bank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Maybank Mock App",
+                description = "Backend Rest API for Maybank Mock App",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Wal Ikram Suaimi",
+                        email = "wsuaimi@gmail.com",
+                        url = "https://github.com/intenzemotion"
+                ),
+                license = @License(
+                        name = "GPL-3.0",
+                        url = "https://github.com/intenzemotion"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Backend Rest API Documentation for Maybank",
+                url = "https://github.com/intenzemotion"
+        )
+)
 public class BankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplication.class, args);
+    }
 
 }
--- src/main/java/com/maybank/bank/controller/UserController.java
+++ src/main/java/com/maybank/bank/controller/UserController.java
@@ -1,15 +1,15 @@
 package com.maybank.bank.controller;
 
-import com.maybank.bank.dto.BankResponse;
-import com.maybank.bank.dto.CreditDebitRequest;
-import com.maybank.bank.dto.EnquiryRequest;
-import com.maybank.bank.dto.UserRequest;
+import com.maybank.bank.dto.*;
 import com.maybank.bank.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     private final UserService userService;
@@ -17,30 +17,80 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creating a new user and assigning an account ID"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @PostMapping
     public BankResponse createAccount(@RequestBody UserRequest userRequest) {
         return userService.createAccount(userRequest);
     }
 
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Given an account number, check how much the user has"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/balanceEnquiry")
     public BankResponse balanceEnquiry(@RequestBody EnquiryRequest enquiryRequest) {
         return userService.balanceEnquiry(enquiryRequest);
     }
 
+    @Operation(
+            summary = "Name Enquiry",
+            description = "Given an account number, check the account user name"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/nameEnquiry")
     public String nameEnquiry(@RequestBody EnquiryRequest enquiryRequest) {
         return userService.nameEnquiry(enquiryRequest);
     }
 
+    @Operation(
+            summary = "Credit Request",
+            description = "Credit a specific amount to an account number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @PostMapping("/credit")
     public BankResponse creditAccount(@RequestBody CreditDebitRequest creditRequest) {
         return userService.creditAccount(creditRequest);
     }
 
+    @Operation(
+            summary = "Debit Request",
+            description = "Debit a specific amount from an account number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @PostMapping("/debit")
     public BankResponse debitAccount(@RequestBody CreditDebitRequest debitRequest) {
         return userService.debitAccount(debitRequest);
     }
+
+    @Operation(
+            summary = "Transfer Request",
+            description = "Transfer a specific amount from a source to a destination account number"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @PostMapping("/transfer")
     public BankResponse transfer(@RequestBody TransferRequest transferRequest) {
         return userService.transfer(transferRequest);
